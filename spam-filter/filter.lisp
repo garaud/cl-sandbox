330c47ad3de838af651f229ad5f722b7e12034ff
Damien Garaud <damien.garaud@gmail.com>
2016-03-17 00:47:45 +0100

    begin to write a spam filter

---
+++ spam-filter/filter.lisp
@@ -0,0 +1,107 @@
+(in-package :spam-filter)
+
+(ql:quickload "cl-ppcre")
+
+(defvar *text* "ceci est un texte de mail, je vous assure, je ne suis pas un mail spam")
+(defvar *feature-database* (make-hash-table :test #'equal))
+(defvar *max-ham-score* 0.4)
+(defvar *min-spam-score* 0.6)
+(defvar *total-spam* 0)
+(defvar *total-ham* 0)
+
+(cl-ppcre:all-matches-as-strings "[a-z]{3,}" *text*)
+
+
+
+(defclass  word-feature ()
+  ((word
+    :initarg :word
+    :accessor word
+    :initform (error "Must supply :word")
+    :documentation "the word this feature represents")
+   (spam-count
+    :initarg :spam-count
+    :accessor spam-count
+    :initform 0
+    :documentation "Spam count")
+   (ham-count
+    :initarg :ham-count
+    :accessor ham-count
+    :initform 0
+    :documentation "Ham count")))
+
+(make-instance 'word-feature :word "jazz")
+
+
+
+(defun clear-database ()
+  "Clear *feature-database*"
+  (setf *feature-database* (make-hash-table :test #'equal)
+        *total-spam* 0
+        *total-ham* 0))
+
+(clear-database)
+
+(defun intern-feature (word)
+  "insert a new word feature into the database from a word"
+  (or (gethash word *feature-database*)
+      (setf (gethash  word *feature-database*)
+            (make-instance 'word-feature :word word))))
+
+(defun extract-words (text)
+  "extract words from a text"
+  (delete-duplicates
+   (cl-ppcre:all-matches-as-strings "[a-zA-Z]{3,}" text)))
+
+(extract-words *text*)
+
+(defun extract-features (text)
+  "extracts all features from a text"
+  (mapcar #'intern-feature (extract-words text)))
+
+(extract-features *text*)
+
+(defmethod print-object ((object word-feature) stream)
+  "proper print word-feature"
+  (print-unreadable-object (object stream :type t)
+    (with-slots (word ham-count spam-count) object
+      (format stream "~s :hams ~d :spams ~d" word ham-count spam-count))))
+
+(defun classify (text)
+  "classify a text as spam or ham"
+  (classification (score (extract-features text))))
+
+(defun classification (score)
+  "is it a spam or a ham?"
+  (cond
+    ((<= score *max-ham-score*) 'ham)
+    ((>= score *min-spam-score*) 'spam)
+    (t 'unsure)))
+
+(classification .9)
+
+(defun train (text type)
+  "train add +1 for ham or spam (type) for each word features in the text"
+  (dolist (feature (extract-features text))
+    (increment-count feature type))
+  (increment-total-count type))
+
+(defun increment-count (feature type)
+  "increment ham or spam, i.e. type"
+  (ecase type
+    (ham (incf (ham-count feature)))
+    (spam (incf (spam-count feature)))))
+
+(defun increment-total-count (type)
+  "increment the total number of ham/spam"
+  (ecase type
+    (ham (incf *total-ham*))
+    (spam (incf *total-spam*))))
+
+
+(train *text* 'ham)
+
+
+(defun spam-probability (feature)
+  (with-slots (spam-count ham-count) feature)
+  (/ spam-count (+ spam-count ham-count)))
