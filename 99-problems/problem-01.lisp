a0d8f6a481e2dc6421d3c15602b64b3224046338
Damien Garaud <damien.garaud@gmail.com>
2017-01-25 00:45:05 +0100

    99-problems: add lisp-unit dep

--- 99-problems/problem-01.lisp
+++ 99-problems/problem-01.lisp
@@ -1,6 +1,10 @@
 ;; 01 find the last element of a list
 
-(in-package #:lisp-problems)
+(in-package :lisp-problems)
+
+(ql:quickload :lisp-unit)
+(use-package :lisp-unit)
+
 
 (defun my-last (seq)
   "last element of a list"
@@ -18,3 +22,8 @@
 
 (my-last seq) ;; D
 (my-last-rec seq) ;; D
+
+
+(define-test test-last
+  (assert-equal (my-last seq) 'd))
+(run-tests :all)
