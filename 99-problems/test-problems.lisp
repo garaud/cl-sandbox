7ed122b66cb83ebcf8210b701acf9173c9dad6ef
Damien Garaud <damien.garaud@gmail.com>
2017-02-07 21:24:07 +0100

    99-problems: swap arguments for 'assert-equal'
    the expected result is the first argument

--- 99-problems/test-problems.lisp
+++ 99-problems/test-problems.lisp
@@ -12,15 +12,15 @@
 ;; # 01: last element
 (define-test test-01-problem
     (let ((seq '(a b c d e)))
-      (assert-equal (my-last seq) 'e)
-      (assert-equal (my-last-rec seq) 'e)))
+      (assert-equal 'e (my-last seq))
+      (assert-equal 'e (my-last-rec seq))))
 
 ;; # 02: last but one
 (define-test test-02-problem
   (let ((seq-a '(a b c d))
         (seq-b '(1 2)))
-    (assert-equal (my-last-but seq-a) 'c)
-    (assert-equal (my-last-but seq-b) 1)
+    (assert-equal 'c (my-last-but seq-a))
+    (assert-equal 1 (my-last-but seq-b))
     (assert-error 'simple-error (my-last-but '()))
     (assert-error 'simple-error (my-last-but '(one)))))
 
@@ -28,30 +28,30 @@
 (define-test test-02-problem-rec
   (let ((seq-a '(a b c d))
         (seq-b '(1 2)))
-    (assert-equal (my-last-but-rec seq-a) 'c)
-    (assert-equal (my-last-but-rec seq-b) 1)
+    (assert-equal 'c (my-last-but-rec seq-a))
+    (assert-equal 1 (my-last-but-rec seq-b))
     (assert-error 'simple-error (my-last-but-rec '()))
     (assert-error 'simple-error (my-last-but-rec '(one)))))
 
 ;; # 03: find the k-th element of a list
 (define-test test-03-element-at
   (let ((seq '(a b c d)))
-    (assert-equal (element-at seq 2) 'c)
+    (assert-equal 'c (element-at seq 2))
     (assert-error 'simple-error (element-at seq 12))
     (assert-error 'simple-error (element-at seq -1))))
 
 ;; # 04: find the number of elements of a list
 (define-test test-04-element-nb
   (let ((seq '(a b c d)))
-    (assert-equal (element-nb seq) 4)
-    (assert-equal (element-nb ()) 0)))
+    (assert-equal 4 (element-nb seq))
+    (assert-equal 0 (element-nb ()))))
 
 ;; # 05: reverse a list
 (define-test test-05-reverse-a-list
   (let ((seq '(a b c d))
         (nested '((list :a :b) (list 1 2) (list t nil))))
-    (assert-equal (my-reverse seq) '(d c b a))
-    (assert-equal (my-reverse nested) '((list t nil) (list 1 2) (list :a :b)))))
+    (assert-equal '(d c b a) (my-reverse seq))
+    (assert-equal '((list t nil) (list 1 2) (list :a :b)) (my-reverse nested))))
 
 ;; # 06: find out whether a list is a palindrome
 (define-test test-06-is-a-palindrome
