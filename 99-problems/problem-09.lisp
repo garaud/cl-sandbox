2f804186f696082ffb9c91253f30e07db8a7082a
Damien Garaud <damien.garaud@gmail.com>
2017-02-11 16:38:26 +0100

    99-problems: ended up the problem no 09
    about the pack function.

--- 99-problems/problem-09.lisp
+++ 99-problems/problem-09.lisp
@@ -5,4 +5,13 @@
 
 (defun pack (seq)
   "pack duplicates elements into sublists"
-  seq)
+  (labels ((nested (seq sub result)
+             (cond
+               ((null seq) (cons sub result))
+               ((equal (first sub) (first seq)) (nested (rest seq) (push (first seq) sub) result))
+               (t (nested (rest seq) (list (first seq)) (push sub result))))))
+    (if (null seq) seq
+        (reverse (nested (rest seq) (list (first seq)) ())))))
+
+;; (pack (list 1 1 2 3 3 3))
+;; (pack (list 1 1 2 3 3 3 4))
