ecb3320c6eba2c81ff4fd206927204cc35110ed4
Damien Garaud <damien.garaud@gmail.com>
2017-02-13 22:58:58 +0100

    99-problems: some test for destructuring-bind for the problem 08

--- 99-problems/problem-08.lisp
+++ 99-problems/problem-08.lisp
@@ -14,4 +14,14 @@
              (append (list y) (uniq (rest (rest seq))))
              (append (list x) (uniq (rest seq))))))))
 
+;; try destructuring-bind or destructuring-case
 ;; (uniq '(a a b c d e f f))
+
+(destructuring-bind (x y &rest z) (list 1 2 3 4 5)
+  (list :x x :y y :z z))
+
+(destructuring-bind (x y &rest z) (list 1 2)
+  (list :x x :y y :z z))
+
+(destructuring-bind (x &optional y &rest z) (list 1)
+  (list :x x :y y :z z))
